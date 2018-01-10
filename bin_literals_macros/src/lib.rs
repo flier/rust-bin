3ff04d2b93dfd1d3089ec54252a00bf71a792323
Flier Lu <flier.lu@gmail.com>
2018-01-10 15:39:56 +0800

    use quote::Tokens to collect elements

--- bin_literals_macros/src/lib.rs
+++ bin_literals_macros/src/lib.rs
@@ -1,8 +1,11 @@
 extern crate bin;
 extern crate byteorder;
+extern crate proc_macro2;
 extern crate proc_macro;
 #[macro_use]
 extern crate procedural_masquerade;
+#[macro_use]
+extern crate quote;
 
 use byteorder::NativeEndian;
 
@@ -15,26 +18,33 @@ define_proc_macros! {
         if is_negative {
             trimmed_input = input[1..].trim_left();
         }
-        match parse_rust_bin_lit::<NativeEndian>(trimmed_input, is_negative) {
-            Ok(value) => format!("const VALUE: &'static [u8] = &{:?};", value.as_slice()),
+
+        let output = match parse_rust_bin_lit::<NativeEndian>(trimmed_input, is_negative) {
+            Ok(value) => {
+                let mut tokens = quote::Tokens::new();
+
+                tokens.append_separated(value, proc_macro2::Term::intern(","));
+
+                quote! {
+                    const VALUE: &'static [u8] = &[ #tokens ];
+                }
+            },
             Err(err) => panic!("{}", err),
-        }
+        };
+
+        output.to_string()
     }
 
     pub fn internal_hex_literals_macros(input: &str) -> String {
-        let mut value = Vec::with_capacity((input.len() + 3) / 4);
-
-        match parse_raw_hex_literal(&mut value, input) {
-            Ok(_) => format!("const VALUE: &'static [u8] = &{:?};", value.as_slice()),
+        match parse_raw_hex_literal(input) {
+            Ok(bytes) => format!("const VALUE: &'static [u8] = &{:?};", bytes.as_slice()),
             Err(err) => panic!("{}", err),
         }
     }
 
     pub fn internal_base64_literals_macros(input: &str) -> String {
-        let mut value = Vec::with_capacity((input.len() + 3) / 4);
-
-        match parse_raw_base64_literal(&mut value, input) {
-            Ok(_) => format!("const VALUE: &'static [u8] = &{:?};", value.as_slice()),
+        match parse_raw_base64_literal(input) {
+            Ok(bytes) => format!("const VALUE: &'static [u8] = &{:?};", bytes.as_slice()),
             Err(err) => panic!("{}", err),
         }
     }
--- src/traits.rs
+++ src/traits.rs
@@ -8,72 +8,59 @@ use failure::Error;
 use byteorder::{BigEndian, ByteOrder};
 use extprim::u128::{BYTES as U128_BYTES, u128};
 use extprim::i128::{BYTES as I128_BYTES, i128};
-use quote::ToTokens;
+use quote::{ToTokens, Tokens};
 use num::FromPrimitive;
 
 /// Parses a Rust literal into an actual binary type.
 ///
 /// If `is_negative` is true, a negative sign will be added to the number before the conversion.
-///
-/// # Examples
-///
-/// ```rust
-/// # extern crate byteorder;
-/// # extern crate bin;
-/// #
-/// # use byteorder::NativeEndian;
-/// #
-/// # use bin::traits::parse_rust_bin_lit;
-/// #
-/// #
-/// # fn main() {
-/// assert_eq!(parse_rust_bin_lit::<NativeEndian>("0, 1", false).unwrap(), &[0, 1]);
-/// assert_eq!(parse_rust_bin_lit::<NativeEndian>("\"你好\"", false).unwrap(),
-///            &[228, 189, 160, 229, 165, 189]);
-/// assert_eq!(parse_rust_bin_lit::<NativeEndian>("0b111", true).unwrap(), &[0xF9]);
-/// assert_eq!(parse_rust_bin_lit::<NativeEndian>("3.14", true).unwrap(),
-///            &[195, 245, 72, 192]);
-/// assert_eq!(parse_rust_bin_lit::<NativeEndian>("000102030405060708090a0b0c0d0e0f", false).unwrap(),
-///            &[0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f]);
-/// # }
-/// ```
-pub fn parse_rust_bin_lit<E>(code: &str, is_negative: bool) -> Result<Vec<u8>, Error>
+pub fn parse_rust_bin_lit<E>(code: &str, is_negative: bool) -> Result<Tokens, Error>
 where
     E: ByteOrder,
 {
-    let mut bytes = vec![];
+    let mut tokens = Tokens::new();
 
     syn::parse_str::<syn::Expr>(code)
         .or_else(|_| syn::parse_str::<syn::Expr>(format!("({})", code).as_str()))
         .map_err(|err| Error::from(err))
-        .and_then(|expr| parse_expr::<E>(&mut bytes, &expr, is_negative))
+        .and_then(|expr| parse_expr::<E>(&mut tokens, &expr, is_negative))
         .or_else(|_| {
             for input in code.split(',').map(|s| s.trim()).filter(|s| !s.is_empty()) {
                 syn::parse_str::<syn::Expr>(input)
                     .or_else(|_| syn::parse_str::<syn::Expr>(format!("({})", code).as_str()))
                     .map_err(|err| Error::from(err))
-                    .and_then(|expr| parse_expr::<E>(&mut bytes, &expr, is_negative))
-                    .or_else(|_| parse_raw_hex_literal(&mut bytes, input))
-                    .or_else(|_| parse_raw_base64_literal(&mut bytes, input))
+                    .and_then(|expr| parse_expr::<E>(&mut tokens, &expr, is_negative))
+                    .or_else(|_| {
+                        parse_raw_hex_literal(input).map(|bytes| {
+                            tokens.append_all(bytes);
+                        })
+                    })
+                    .or_else(|_| {
+                        parse_raw_base64_literal(input).map(|bytes| {
+                            tokens.append_all(bytes);
+                        })
+                    })
                     .or_else(|_| bail!("syntax error: {}", input))?;
             }
 
             Ok(())
         })
-        .map(|_| bytes)
+        .map(|_| tokens)
 }
 
-fn parse_expr<E>(bytes: &mut Vec<u8>, expr: &syn::Expr, is_negative: bool) -> Result<(), Error>
+fn parse_expr<E>(tokens: &mut Tokens, expr: &syn::Expr, is_negative: bool) -> Result<(), Error>
 where
     E: ByteOrder,
 {
     match *expr {
         syn::Expr::Lit(syn::ExprLit { ref lit, .. }) => {
-            parse_lit_expr::<E>(bytes, lit, is_negative)
+            parse_lit_expr::<E>(lit, is_negative).map(|bytes| {
+                tokens.append_all(bytes);
+            })
         }
         syn::Expr::Repeat(syn::ExprRepeat {
             ref expr, ref len, ..
-        }) => parse_repeat_expr::<E>(bytes, expr, len),
+        }) => parse_repeat_expr::<E>(tokens, expr, len),
         syn::Expr::Range(syn::ExprRange {
             ref from,
             limits,
@@ -81,34 +68,43 @@ where
             ..
         }) if from.is_some() && to.is_some() =>
         {
-            parse_range_expr::<E>(bytes, from.as_ref().unwrap(), limits, to.as_ref().unwrap())
+            parse_range_expr::<E>(from.as_ref().unwrap(), limits, to.as_ref().unwrap()).map(
+                |bytes| {
+                    tokens.append_all(bytes);
+                },
+            )
         }
         syn::Expr::Array(syn::ExprArray { ref elems, .. })
         | syn::Expr::Tuple(syn::ExprTuple { ref elems, .. }) => {
             for element in elems {
-                parse_expr::<E>(bytes, element, false)?;
+                parse_expr::<E>(tokens, element, false)?;
             }
 
             Ok(())
         }
-        syn::Expr::Paren(syn::ExprParen { ref expr, .. }) => parse_expr::<E>(bytes, expr, false),
-        syn::Expr::Unary(syn::ExprUnary { op, ref expr, .. }) => {
-            parse_unary_expr::<E>(bytes, op, expr)
-        }
+        syn::Expr::Paren(syn::ExprParen { ref expr, .. }) => parse_expr::<E>(tokens, expr, false),
+        syn::Expr::Unary(syn::ExprUnary { op, ref expr, .. }) => parse_unary_expr::<E>(op, expr)
+            .map(|bytes| {
+                tokens.append_all(bytes);
+            }),
         syn::Expr::Binary(syn::ExprBinary {
             ref left,
             op,
             ref right,
             ..
-        }) => parse_bin_expr::<E>(bytes, left, op, right),
+        }) => parse_bin_expr::<E>(left, op, right).map(|bytes| {
+            tokens.append_all(bytes);
+        }),
         ref expr => bail!("unsupport expr, {:?}", expr),
     }
 }
 
-fn parse_lit_expr<E>(bytes: &mut Vec<u8>, lit: &syn::Lit, is_negative: bool) -> Result<(), Error>
+fn parse_lit_expr<E>(lit: &syn::Lit, is_negative: bool) -> Result<Vec<u8>, Error>
 where
     E: ByteOrder,
 {
+    let mut bytes = vec![];
+
     match *lit {
         syn::Lit::Str(ref s) => {
             bytes.put(s.value());
@@ -245,49 +241,48 @@ where
         }
     }
 
-    Ok(())
+    Ok(bytes)
 }
 
-fn parse_repeat_expr<E>(bytes: &mut Vec<u8>, expr: &syn::Expr, len: &syn::Expr) -> Result<(), Error>
+fn parse_repeat_expr<E>(tokens: &mut Tokens, expr: &syn::Expr, len: &syn::Expr) -> Result<(), Error>
 where
     E: ByteOrder,
 {
     let len = eval_const_expr_as_num::<usize>(&len)?;
-    let mut value = vec![];
+    let mut value = Tokens::new();
 
     parse_expr::<E>(&mut value, &expr, false)?;
 
     for _ in 0..len {
-        bytes.extend_from_slice(&value);
+        tokens.append_all(value.clone());
     }
 
     Ok(())
 }
 
 fn parse_range_expr<E>(
-    bytes: &mut Vec<u8>,
     from: &syn::Expr,
     limits: syn::RangeLimits,
     to: &syn::Expr,
-) -> Result<(), Error>
+) -> Result<Vec<u8>, Error>
 where
     E: ByteOrder,
 {
     let from = eval_const_expr_as_num::<u16>(from)?;
     let to = eval_const_expr_as_num::<u16>(to)?;
 
-    match limits {
-        syn::RangeLimits::HalfOpen(_) => bytes.extend((from..to).map(|b| b as u8)),
-        syn::RangeLimits::Closed(_) => bytes.extend((from..to + 1).map(|b| b as u8)),
-    }
-
-    Ok(())
+    Ok(match limits {
+        syn::RangeLimits::HalfOpen(_) => (from..to).map(|b| b as u8).collect(),
+        syn::RangeLimits::Closed(_) => (from..to + 1).map(|b| b as u8).collect(),
+    })
 }
 
-fn parse_unary_expr<E>(bytes: &mut Vec<u8>, op: syn::UnOp, expr: &syn::Expr) -> Result<(), Error>
+fn parse_unary_expr<E>(op: syn::UnOp, expr: &syn::Expr) -> Result<Vec<u8>, Error>
 where
     E: ByteOrder,
 {
+    let mut bytes = vec![];
+
     match op {
         syn::UnOp::Neg(_) => {
             let n = eval_const_expr_as_num::<i64>(expr)?;
@@ -322,18 +317,15 @@ where
         _ => bail!("unsupport unary op: {:?}", op),
     }
 
-    Ok(())
+    Ok(bytes)
 }
 
-fn parse_bin_expr<E>(
-    bytes: &mut Vec<u8>,
-    left: &syn::Expr,
-    op: syn::BinOp,
-    right: &syn::Expr,
-) -> Result<(), Error>
+fn parse_bin_expr<E>(left: &syn::Expr, op: syn::BinOp, right: &syn::Expr) -> Result<Vec<u8>, Error>
 where
     E: ByteOrder,
 {
+    let mut bytes = vec![];
+
     if let Some(v) =
         match op {
             syn::BinOp::Add(_) => eval_const_expr_as_num::<u64>(left)?
@@ -374,7 +366,7 @@ where
         bail!("binary op overflow: {:?}", op)
     };
 
-    Ok(())
+    Ok(bytes)
 }
 
 fn eval_const_expr_as_num<T>(expr: &syn::Expr) -> Result<T, Error>
@@ -400,11 +392,12 @@ where
     bail!("unsupport expr, {:?}", expr)
 }
 
-pub fn parse_raw_hex_literal(bytes: &mut Vec<u8>, input: &str) -> Result<(), Error> {
+pub fn parse_raw_hex_literal(input: &str) -> Result<Vec<u8>, Error> {
     if input.len() % 2 == 0 && input.chars().all(|c| match c {
         '0'...'9' | 'a'...'f' | 'A'...'F' => true,
         _ => false,
     }) {
+        let mut bytes = vec![];
         let mut chars = input.chars();
 
         while let (Some(hi), Some(lo)) = (
@@ -414,7 +407,7 @@ pub fn parse_raw_hex_literal(bytes: &mut Vec<u8>, input: &str) -> Result<(), Err
             bytes.push(((hi << 4) + lo) as u8);
         }
 
-        Ok(())
+        Ok(bytes)
     } else {
         bail!("illegel hex literal");
     }
@@ -424,12 +417,13 @@ const BASE64_CHUNK_SIZE: usize = 4;
 const BASE64_INDEX_TABLE: &[u8; 64] =
     b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-pub fn parse_raw_base64_literal(bytes: &mut Vec<u8>, input: &str) -> Result<(), Error> {
+pub fn parse_raw_base64_literal(input: &str) -> Result<Vec<u8>, Error> {
     if (input.as_bytes().len() % BASE64_CHUNK_SIZE) != 1
         && input.as_bytes().iter().all(|&b| match b {
             b'A'...b'Z' | b'a'...b'z' | b'0'...b'9' | b'+' | b'/' | b'=' => true,
             _ => false,
         }) {
+        let mut bytes = vec![];
         let mut chunks = input.as_bytes().chunks(BASE64_CHUNK_SIZE);
 
         while let Some(chunk) = chunks.next() {
@@ -454,7 +448,7 @@ pub fn parse_raw_base64_literal(bytes: &mut Vec<u8>, input: &str) -> Result<(), 
             bytes.extend(v)
         }
 
-        Ok(())
+        Ok(bytes)
     } else {
         bail!("illegel base64 literal");
     }
@@ -466,16 +460,10 @@ mod tests {
 
     #[test]
     fn test_parse_raw_hex_literal() {
-        let mut v = vec![];
-
-        assert!(
-            parse_raw_hex_literal(
-                &mut v,
-                "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
-            ).is_ok()
-        );
         assert_eq!(
-            &v,
+            &parse_raw_hex_literal(
+                "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
+            ).unwrap(),
             &[
                 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d,
                 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b,
@@ -483,27 +471,17 @@ mod tests {
             ]
         );
 
-        v.clear();
-
-        assert!(parse_raw_hex_literal(&mut v, "").is_ok());
-        assert!(v.is_empty());
-
-        assert!(parse_raw_hex_literal(&mut v, "123").is_err());
-        assert!(parse_raw_hex_literal(&mut v, "123+").is_err());
+        assert!(parse_raw_hex_literal("").unwrap().is_empty());
+        assert!(parse_raw_hex_literal("123").is_err());
+        assert!(parse_raw_hex_literal("123+").is_err());
     }
 
     #[test]
     fn test_parse_raw_base64_literal() {
-        let mut v = vec![];
-
-        assert!(
-            parse_raw_base64_literal(
-                &mut v,
-                "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4="
-            ).is_ok()
-        );
         assert_eq!(
-            v.as_slice(),
+            parse_raw_base64_literal(
+                "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4="
+            ).unwrap().as_slice(),
             &b"Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure."[..]
         );
 
@@ -516,17 +494,18 @@ mod tests {
         //
         // For example:
 
-        v.clear();
-        assert!(parse_raw_base64_literal(&mut v, "YW55IGNhcm5hbCBwbGVhcw==").is_ok());
-        assert_eq!(v.as_slice(), b"any carnal pleas");
-
-        v.clear();
-        assert!(parse_raw_base64_literal(&mut v, "YW55IGNhcm5hbCBwbGVhc3U=").is_ok());
-        assert_eq!(v.as_slice(), b"any carnal pleasu");
-
-        v.clear();
-        assert!(parse_raw_base64_literal(&mut v, "YW55IGNhcm5hbCBwbGVhc3Vy").is_ok());
-        assert_eq!(v.as_slice(), b"any carnal pleasur");
+        assert_eq!(
+            &parse_raw_base64_literal("YW55IGNhcm5hbCBwbGVhcw==").unwrap(),
+            b"any carnal pleas"
+        );
+        assert_eq!(
+            &parse_raw_base64_literal("YW55IGNhcm5hbCBwbGVhc3U=").unwrap(),
+            b"any carnal pleasu"
+        );
+        assert_eq!(
+            &parse_raw_base64_literal("YW55IGNhcm5hbCBwbGVhc3Vy").unwrap(),
+            b"any carnal pleasur"
+        );
 
         // Decoding Base64 without padding
         //
@@ -538,16 +517,17 @@ mod tests {
         //
         // For example:
 
-        v.clear();
-        assert!(parse_raw_base64_literal(&mut v, "YW55IGNhcm5hbCBwbGVhcw").is_ok());
-        assert_eq!(v.as_slice(), b"any carnal pleas");
-
-        v.clear();
-        assert!(parse_raw_base64_literal(&mut v, "YW55IGNhcm5hbCBwbGVhc3U").is_ok());
-        assert_eq!(v.as_slice(), b"any carnal pleasu");
-
-        v.clear();
-        assert!(parse_raw_base64_literal(&mut v, "YW55IGNhcm5hbCBwbGVhc3Vy").is_ok());
-        assert_eq!(v.as_slice(), b"any carnal pleasur");
+        assert_eq!(
+            &parse_raw_base64_literal("YW55IGNhcm5hbCBwbGVhcw").unwrap(),
+            b"any carnal pleas"
+        );
+        assert_eq!(
+            &parse_raw_base64_literal("YW55IGNhcm5hbCBwbGVhc3U").unwrap(),
+            b"any carnal pleasu"
+        );
+        assert_eq!(
+            &parse_raw_base64_literal("YW55IGNhcm5hbCBwbGVhc3Vy").unwrap(),
+            b"any carnal pleasur"
+        );
     }
 }
